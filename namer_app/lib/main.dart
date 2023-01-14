2e2c1bb6abde138aa215013e3d1fcea84b9f8fa1
Colene Encarnado <encarnac@oregonstate.edu>
2023-01-14 06:45:47 -0800

    Cleaned up comments

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -2,14 +2,16 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// Tells Flutter to run the app defined in MyApp.
+/// Function: main
+///   Tells Flutter to run the app defined in MyApp.
 void main() {
   runApp(MyApp());
 }
 
-// The MyApp class extends StatelessWidget. Widgets are the elements from which you build every Flutter app.
-// MyApp sets up the whole app. It creates the app-wide state (more on this later), names the app,
-//defines the visual theme, and sets the "home" widget—the starting point of your app.
+/// Widget: MyApp
+/// StatelessWidget - creates the app-wide state, names the app,
+///   defines the visual theme, and sets the "home" widget—the starting
+///   point of your app.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -29,47 +31,51 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// MyAppState class defines the app's...well...state.
-// ChangeNotifier, which means that it can notify others about its own changes.
-// The state is created and provided to the whole app using a ChangeNotifierProvider (see code above in MyApp).
-//This allows any widget in the app to get hold of the state.
+/// Class: MyAppState
+/// ChangeNotifier - Defines the app's state. Notifies others about its changes.
+///   The state is created and provided to the whole app using a ChangeNotifierProvider
+///   (see code above in MyApp). This allows any widget in the app to get hold of the state.
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
   void getNext() {
-    current = WordPair
-        .random(); // getNext() method reassigns current with a new random WordPair.
-    notifyListeners(); //  Calls notifyListeners()(a method of ChangeNotifier)that ensures that anyone watching MyAppState is notified.
+    // getNext() method reassigns current with a new random WordPair.
+    current = WordPair.random();
+
+    // Notifies MyAppState listeners of a change
+    notifyListeners();
   }
 }
 
-// The Home widget set
+/// Widget: MyHomePage
+///
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Every widget defines a build() method that's automatically called every time the widget's circumstances change so that the widget is always up to date.
-    var appState = context.watch<
-        MyAppState>(); // Tracks changes to the app's current state using the watch method.
-    var pair = appState
-        .current; // Make sure that the line being extracted only accesses what it needs
+    // Tracks changes to MyAppState to rebuild the widget
+    var appState = context.watch<MyAppState>();
 
+    // Extracts only the value needed (current word pair)
+    var pair = appState.current;
+
+    // Returns the Scaffold widget (the basic Material visual layout structure)
     return Scaffold(
-      // Build methods must return a widget or (more typically) a nested tree of widgets. In this case, the top-level widget is Scaffold (commonly used)
+      // Column widget takes any number of children and displays them vertically.
       body: Column(
-        // Column is one of the most basic layout widgets in Flutter. It takes any number of children and puts them in a column from top to bottom. By default, the column visually places its children at the top.
         children: [
-          Text(
-              'A random AWESOME idea:'), // You changed this Text widget in the first step.
-          BigCard(
-              pair:
-                  pair), // This second Text widget takes appState, and accesses the only member of that class, current (which is a WordPair). WordPair provides several helpful getters, such as asPascalCase or asSnakeCase. Here, we use asLowerCase but you can change this now if you prefer one of the alternatives.
+          // First child is a Text widget (a run of text with a single style)
+          Text('A random AWESOME idea:'),
+          // Second child is a reference to an imported widget containing the current word pair
+          BigCard(pair: pair),
+          // Third child is a button widget that calls the getNext() function to change current word
           ElevatedButton(
             onPressed: () {
               appState.getNext();
             },
             child: Text('Next'),
+            // Add trailing commas for easy append and for Dart's auto newline
           ),
-        ], // It is generally a good idea to use trailing commas: they make adding more members trivial, and they also serve as a hint for Dart's auto-formatter to put a newline there.
+        ],
       ),
     );
   }
@@ -93,13 +99,15 @@ class BigCard extends StatelessWidget {
     var style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
     );
-pair
+
     return Card(
       // Defines the card's color to be the same as the theme's colorScheme property.
-      color: theme.colorScheme.primary, 
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: style),
+        child: Text(pair.asLowerCase,
+            style:
+                style), // This second Text widget takes appState, and accesses the only member of that class, current (which is a WordPair). WordPair provides several helpful getters, such as asPascalCase or asSnakeCase. Here, we use asLowerCase but you can change this now if you prefer one of the alternatives
       ),
     );
   }
