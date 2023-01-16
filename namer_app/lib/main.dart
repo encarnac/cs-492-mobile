40da0985a0f3b61c256cccde7131cbcaf680a56b
Colene Encarnado <encarnac@oregonstate.edu>
2023-01-16 05:27:36 -0800

    Completed tutorial and added remove from favorites

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -35,20 +35,106 @@ class MyApp extends StatelessWidget {
 ///   The state is created and provided to the whole app by extending ChangeNotifierProvider
 ///   (see code above in MyApp). This allows any widget in the app to get hold of the state.
 class MyAppState extends ChangeNotifier {
+  // Stores the current WordPair
   var current = WordPair.random();
 
+  // Reassigns current with a new random WordPair and notifies listeners
   void getNext() {
-    // Reassigns current with a new random WordPair.
     current = WordPair.random();
+    notifyListeners();
+  }
 
-    // Notifies MyAppState listeners of a change
+  // Stores a list of saved WordPairs
+  var favorites = <WordPair>[];
+
+  // Removes existing WordPair from list or adds it if new, then notifies listeners
+  void toggleFavorite(value) {
+    if (favorites.contains(value)) {
+      favorites.remove(value);
+    } else {
+      favorites.add(value);
+    }
     notifyListeners();
   }
 }
 
 /// Widget: MyHomePage
+///   Contains the navigation sidebar for the Home and Favorites pages
+///   Stateful because it only tracks state relevant to it (selectedIndex)
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Stores the current navigation tab (0 = first destination listed in NavigationRail)
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // Create page widget and assigns screen content based on selectedIndex
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    // Build methods must return a widget (Scaffold - top-level layout widget)
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            // Child 1 - SafeArea ensures that its child (NavigationRail) is not obscured by system UI
+            SafeArea(
+              child: NavigationRail(
+                // Sets label visibility next to icons
+                extended: constraints.maxWidth >= 500,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+
+                // The current navigation tab selected
+                selectedIndex: selectedIndex,
+
+                // Defines what happens when destination icon selected
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+
+            // Child 2 - Expanded widgets set children greedy for available axis space
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+/// Widget: GeneratorPage
 ///   Contains the layout structure consisting of nested widgets
-class MyHomePage extends StatelessWidget {
+class GeneratorPage extends StatelessWidget {
   // Rebuilds widget everytime dependencies change
   @override
   Widget build(BuildContext context) {
@@ -58,32 +144,55 @@ class MyHomePage extends StatelessWidget {
     // Extracts only the attribute needed from class (current word pair)
     var pair = appState.current;
 
-    // Build methods must return a widget (Scaffold - top-level structure)
-    return Scaffold(
-      // Style widget to group child widgets in middle of horizontal axis
-      body: Center(
-        // Column - basic layout widget, takes multiple children and displays them vertically
-        child: Column(
-          // Centers child widgets in middle of vertical axis
-          mainAxisAlignment: MainAxisAlignment.center,
+    // Sets icon based on if WordPair is in list
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
 
-          children: [
-            // 1st child - styled container widget holding current WordPair
-            BigCard(pair: pair),
+    // Style widget to group child widgets in middle of horizontal axis
+    return Center(
+      // Column - basic layout widget, takes multiple children and displays them vertically
+      child: Column(
+        // Centers child widgets in middle of vertical axis
+        mainAxisAlignment: MainAxisAlignment.center,
 
-            // 2nd child - Empty style widget to create visual gap
-            SizedBox(height: 10),
+        children: [
+          // 1st child - styled container widget holding current WordPair
+          BigCard(pair: pair),
 
-            // 3rd child - Button widget that calls the getNext() function for new random WordPair
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
-              // Trailing commas for easy append and for Dart's auto newline
-            ),
-          ],
-        ),
+          // 2nd child - Empty style widget to create visual gap
+          SizedBox(height: 10),
+
+          // 3rd child - a row of buttons
+          Row(
+            // Sets row to not take up all available horizontal space
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Button calls toggleFavorite() to add/remove WordPair from list
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite(pair);
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+
+              SizedBox(width: 10.0),
+
+              // Button calls the getNext() function for new random WordPair
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+                // Trailing commas for easy append and for Dart's auto newline
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -126,6 +235,42 @@ class BigCard extends StatelessWidget {
           semanticsLabel: pair.asPascalCase,
         ),
       ),
+    );
+  }
+}
+
+
+/// Page Widget: FavoritesPage
+///   
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            // leading: Icon(Icons.favorite),
+            leading: IconButton(
+                icon: Icon(Icons.favorite),
+                onPressed: () {
+                  appState.toggleFavorite(pair);
+                }),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
